0aa9db7d855f0ae291aa9b4f745ba96d95456001
stus <143853974+StusBesieger@users.noreply.github.com>
2025-05-18 19:13:16 +0900

    Add files via upload

--- src/TBCStusAseem/AdSpotModule.cs
+++ src/TBCStusAseem/AdSpotModule.cs
@@ -32,23 +32,50 @@ namespace TBCStusSpace
     }
     public class TBCAddSpotBehaviour : BlockModuleBehaviour<TBCAddSpotModule>
     {
-        public int blockID;
-        private int Spotnum = 0;
-        public string SpotEffectName;
-        private Vector3 ThisDirection;
+        //エフェクト
         public GameObject EffectPrefab;
         public GameObject EffectObject;
         public ParticleSystem particleSystem;
+        public Vector3 Effectposition;
+
+        //メッセージ、GUI
+        public BlockBehaviour blockBehaviour;
+        private bool isOwnerSame = false;
+
+
+        //プレイヤー操作
         public MKey SpotStart;
-        public Collider mCollider;
-        public float Range; 
+        public bool Spotok = true;
+        public bool SpotReloading = true;
+        public float Range;
         public LayerMask Blocklayermask = (1 << 0) | (1 << 12) | (1 << 14) | (1 << 25) | (1 << 26);
+        public Vector3 ThisDirection;
         private RaycastHit hit;
-        private bool Spottrue = false;
-        private string SpotReload;
-        public bool isOwnerSame = false;
-        private int windowId;
-        private Rect windowRect = new Rect(375, 875, 100,50);
+
+        //メッセージ
+        public int DictID = 0;
+        public bool Spotgetmessage = false;
+
+        private bool Clientbool = false;
+        public void Awake()
+        {
+            blockBehaviour = this.GetComponent<BlockBehaviour>();
+
+            if (blockBehaviour.isBuildBlock)
+            {
+                while (Mod.RangeboolDict.ContainsKey(DictID))
+                {
+                    DictID++;
+                }
+                Mod.SpotEffecboolDict.Add(DictID, value: false);
+                Mod.SpotPositionDict.Add(DictID, value: new Vector3(0f,0f,0f));
+            }
+        }
+        public override void OnBlockPlaced()
+        {
+            base.OnBlockPlaced();
+
+        }
         public override void OnSimulateStart()
         {
             base.OnSimulateStart();
@@ -59,105 +86,126 @@ namespace TBCStusSpace
             EffectObject.transform.position = BlockBehaviour.GetCenter();
 
             Range = Module.SpotRange;
-
+            SpotStart = GetKey(Module.SpotKey);
             UpdateOwnerFlag();
+        }
+        public override void OnSimulateStop()
+        {
+            base.OnSimulateStop();
+            isOwnerSame = false;
+            Mod.SpotEffecboolDict.Remove(DictID);
+            Mod.SpotPositionDict.Remove(DictID);
+        }
+        public override void SimulateFixedUpdateHost()
+        {
+            base.SimulateFixedUpdateHost();
+            if (Spotok)
+            {
+                ThisDirection = -transform.up;
+                if (Physics.SphereCast(this.transform.position + 3f * ThisDirection, 0.25f, ThisDirection, out hit, Range, Blocklayermask))
+                {
+                    Debug.Log("ray");
+                    if(SpotReloading)
+                    {
+                        StartCoroutine(Spot());
+                    }
+                }
+                
+                StartCoroutine(SpotReload());
+            }
+        }
+        public override void SimulateUpdateAlways()
+        {
+            base.SimulateUpdateAlways();
+            if(SpotStart.IsPressed || SpotStart.EmulationPressed())
+            {
+                Spotok = true;
+            }
+
+        }
+        public override void SimulateUpdateClient()
+        {
+            base.SimulateUpdateClient();
+            Clientbool = Mod.SpotEffecboolDict[DictID];
+            if(Clientbool)
+            {
+                if (!EffectObject.activeInHierarchy)
+                {
+                    EffectObject.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                    EffectObject.SetActive(true);
+                    StartCoroutine(SpotReload());
+                }
+                EffectObject.transform.position = Mod.SpotPositionDict[DictID];
+            }
+            else
+            {
+                if(EffectObject.activeInHierarchy)
+                {
+                    EffectObject.SetActive(false);
+                }
+            }
+
+            
+
+        }
+        public override void SimulateUpdateHost()
+        {
+            base.SimulateUpdateHost();
+            if (Spotgetmessage)
+            {
+                EffectObject.transform.position = hit.transform.position;
+                EffectObject.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                Mod.SpotPositionmessage = Mod.SpotPositionmessageType.CreateMessage(DictID, EffectObject.transform.position);
+                ModNetworking.SendToAll(Mod.SpotPositionmessage);
+            }
+            Mod.Spotboolmessage = Mod.SpotboolmessageTyope.CreateMessage(DictID, Spotgetmessage);
+            ModNetworking.SendToAll(Mod.Spotboolmessage);
         }
         private void UpdateOwnerFlag()  //プレイヤーとブロックの親が一緒か確認する関数
         {
             if (StatMaster.isMP)
             {
-                ushort BlockPlayerID = BlockBehaviour.ParentMachine.PlayerID;
-                ushort LocalPlayerID = PlayerMachine.GetLocal().Player.NetworkId;
-                isOwnerSame = BlockPlayerID == LocalPlayerID;
+                ushort num;
+                try
+                {
+                    num = base.BlockBehaviour.ParentMachine.PlayerID;
+                }
+                catch
+                {
+                    num = 50;
+                }
+                ushort num2;
+                try
+                {
+                    num2 = PlayerMachine.GetLocal().Player.NetworkId;
+                }
+                catch
+                {
+                    num2 = 100;
+                }
+                isOwnerSame = num == num2;
             }
             else
             {
                 isOwnerSame = true;
             }
         }
-        public override void OnSimulateStop()
+        IEnumerator Spot()
         {
-            base.OnSimulateStop();
-            isOwnerSame = false;
-        }
-        public override void SafeAwake()
-        {
-            base.SafeAwake();
-            windowId = ModUtility.GetWindowId();
-            blockID = BlockId;
-            try
-            {
-                SpotStart = GetKey(Module.SpotKey);
-            }
-            catch
-            {
-                Mod.Error("BlockID" + blockID + "error");
-            }
-        }
-        //キーを押されたとき、前方にスポットエフェクトを呼び出す。
-        public override void SimulateUpdateAlways()
-        {
-            base.SimulateUpdateAlways();
-            ThisDirection = - transform.up ;
-            if (SpotStart.IsPressed || SpotStart.EmulationPressed())
-            {
-                if(Spotnum == 0)
-                {
-                    if (Physics.SphereCast(this.transform.position + 3f * ThisDirection, 0.25f, ThisDirection, out hit, Range, Blocklayermask))
-                    {
-                        StartCoroutine(PlaySpot());
-
-                    }
-                    Spotnum = 1;
-                }
-            }
-        }
-        public IEnumerator PlaySpot()
-        {
-            EffectObject.transform.position = hit.transform.position;
-            Spottrue = true;
             yield return new WaitForSeconds(1f);
+            Spotgetmessage = true;
             particleSystem.Play();
             yield return new WaitForSeconds(10f);
             particleSystem.Stop();
-            Spottrue = false;
+            Spotgetmessage = false;
         }
-        public override void SimulateFixedUpdateAlways()
+        IEnumerator SpotReload()
         {
-            base.SimulateFixedUpdateAlways();
-            if(Spottrue)
-            {
-                EffectObject.transform.position = hit.transform.position;
-                EffectObject.transform.rotation = Quaternion.Euler(90f,0f,0f);
-
-            }
-            if (Spotnum != 0)
-            {
-                Spotnum++;
-            }
-            if (Spotnum == 500)
-            {
-                Spotnum = 0;
-            }
-            if (0 != Spotnum)
-            {
-                SpotReload = (Spotnum / 5).ToString() + " %";
-            }
-            if (0 == Spotnum)
-            {
-                SpotReload = "Spot OK";
-            }
-        }
-        public void OnGUI()
-        {
-            if(isOwnerSame )
-            {
-                windowRect = GUILayout.Window(windowId, windowRect, delegate (int windowId)
-                {
-                    GUILayout.Label(SpotReload);
-                }
-                , "Spot Reload");
-            }
+            yield return new WaitForFixedUpdate();
+            Spotok = false;
+            SpotReloading = false;
+            yield return new WaitForSeconds(5f);
+            SpotReloading = true;
         }
     }
 }
